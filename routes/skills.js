d4042bb548ad91b13eabbb9295a13b753273a05c
janicavc <crisostomo_janica@yahoo.com>
2023-06-29 08:51:13 -0700

    add ctrls

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const skillsCtrl = require('../controllers/skills');
+
+// GET /skills
+router.get('/', skillsCtrl.index)
 
 module.exports = router;
